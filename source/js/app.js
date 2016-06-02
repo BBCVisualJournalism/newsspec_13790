d62fc61dc18b318b3232ec8a9c053e81a4a675f9
Scott Jarvis <scott.jarvis@bbc.co.uk>
2016-06-02 17:00:15 +0100

    enabled sharetools

--- source/js/app.js
+++ source/js/app.js
@@ -10,44 +10,44 @@ define(['wrapper', 'ShareTools', 'ShareToolsTemplate', 'buttons', 'jquery', 'vie
 
     // exampleButtons.init();
 
-    // var shareTitle   = 'Share this content',
-    //     shareMessage = 'This is my share message',
-    //     config = {
-    //         holderEl: '.tmpShareToolsHolder',
-    //         label: 'Share this page',
-    //         shareUrl: wrapper.url().hostUrl,
-    //         messages: {
-    //             twitter: shareMessage,
-    //             facebook: {
-    //                 title:       shareTitle,
-    //                 description: shareMessage,
-    //                 image:       'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13790/content/iframe/common/img/promo.jpg' // optional
-    //             },
-    //             email: {
-    //                 subject: shareTitle,
-    //                 message: shareMessage
-    //             },
-    //             app: {
-    //                 shareEndpoint: 'bbcnewsapp://visualjournalism/share',
-    //                 popup: false,
-    //                 properties: {
-    //                     title: shareTitle,
-    //                     text: shareMessage
-    //                 }
-    //             }
-    //         },
-    //         template: ShareTemplate
-    //     };
+    var shareTitle   = 'Share this content',
+        shareMessage = 'This is my share message',
+        config = {
+            holderEl: '.tmpShareToolsHolder',
+            label: 'Share this page',
+            shareUrl: wrapper.url().hostUrl,
+            messages: {
+                twitter: shareMessage,
+                facebook: {
+                    title:       shareTitle,
+                    description: shareMessage,
+                    image:       'http://www.stage.bbc.co.uk/news/special/2016/newsspec_13790/content/iframe/common/img/promo.jpg' // optional
+                },
+                email: {
+                    subject: shareTitle,
+                    message: shareMessage
+                },
+                app: {
+                    shareEndpoint: 'bbcnewsapp://visualjournalism/share',
+                    popup: false,
+                    properties: {
+                        title: shareTitle,
+                        text: shareMessage
+                    }
+                }
+            },
+            template: ShareTemplate
+        };
 
-    // if (wrapper.wrapper === 'app') {
-    //     // we often want to deliver a different share view to the app
-    //     config.template = '\
-    //         <div class='share ns__share ns__share-dropdown ns__share--app'>\
-    //             <a class='share__button share__png_icon share__tool--network' data-network='app' href='#'></a>\
-    //         </div>';
-    // }
+    if (wrapper.wrapper === 'app') {
+        // we often want to deliver a different share view to the app
+        config.template = '\
+            <div class="share ns__share ns__share-dropdown ns__share--app">\
+                <a class="share__button share__png_icon share__tool--network" data-network="app" href="#"></a>\
+            </div>';
+    }
 
-    // var shareObject = new ShareTools(config);
+    var shareObject = new ShareTools(config);
 
     // wrapper.onOptimizedScroll(function (scrollTop) {
     //     console.log('Optimized scroll.', scrollTop);
