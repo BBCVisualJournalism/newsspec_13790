9ace05b1f12c8c8e5e34f7f7c1705bdb63df41f8
Scott Jarvis <scott.jarvis@bbc.co.uk>
2016-07-15 17:13:25 +0100

    hide credits section for non-english pages

--- source/js/app.js
+++ source/js/app.js
@@ -3,6 +3,13 @@ define(['wrapper', 'istats', 'jquery', 'view'],
 
     $('.bbc-news-vj-wrapper').addClass('bbc-news-vj-wrapper-js-enabled');
 
+    //show credits only for english page
+    if( $('.bbc-news-vj-wrapper').hasClass('bbc-news-vj-language--news') ){
+        $('.ns_creditsHolder').removeClass('hidden');
+    } else {
+        $('.ns_creditsHolder').addClass('hidden');
+    }
+
     // check if in app, if so, append '.app' to all href urls..
 
     // create the view - to handle inputs, UI changes, etc
